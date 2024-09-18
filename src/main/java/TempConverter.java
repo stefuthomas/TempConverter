e46b4925332cfda431d5bd8be07f204a36f36609
Stefanos Thomas <stefanos.thomas@metropolia.fi>
2024-09-18 12:49:27 +0300

    Initial commit

---
+++ src/main/java/TempConverter.java
@@ -0,0 +1,17 @@
+public class TempConverter {
+    public double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    public double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public boolean isExtremeTemperature(double celsius) {
+        return celsius < -40 || celsius > 50;
+    }
+
+    public static float kelvinToCel(float kel) {
+        return (float) (kel - 273.15);
+    }
+}
---
+++ src/test/java/TempConverterTest.java
@@ -0,0 +1,29 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TempConverterTest {
+    TempConverter converter = new TempConverter();
+    @org.junit.jupiter.api.Test
+    void fahrenheitToCelsius() {
+        assertEquals(0, converter.fahrenheitToCelsius(32));
+        assertEquals(100, converter.fahrenheitToCelsius(212));
+    }
+
+    @org.junit.jupiter.api.Test
+    void celsiusToFahrenheit() {
+        assertEquals(32, converter.celsiusToFahrenheit(0));
+        assertEquals(212, converter.celsiusToFahrenheit(100));
+    }
+
+    @org.junit.jupiter.api.Test
+    void isExtremeTemperature() {
+        assertTrue(converter.isExtremeTemperature(-41));
+        assertTrue(converter.isExtremeTemperature(51));
+        assertFalse(converter.isExtremeTemperature(-40));
+        assertFalse(converter.isExtremeTemperature(50));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testKelvinToCel() {
+        assertEquals(0.0, converter.kelvinToCel(273.15F), 0.001);
+    }
+}
